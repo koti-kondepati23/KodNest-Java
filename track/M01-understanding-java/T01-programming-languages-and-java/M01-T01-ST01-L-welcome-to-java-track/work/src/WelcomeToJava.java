363563c240d6b51ed8e2a46fd6644bba4dd7f206
KONDEPATI KOTESWARA RAO <kotikondepati950@gmail.com>
2026-07-30 13:04:15 +0530

    changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -4,5 +4,6 @@ public class WelcomeToJava {
         System.out.println("I already know how to build programming logic");
         System.out.println("Now I am learning how to express that logic using java");
         System.out.println("My goal is become a java developer");
+        System.out.println("I selected java because of i need to build the large applications");
     }
 }
