6f11ce79c8a5dc2914d6e5e1fb9f00eab8f0679f
KONDEPATI KOTESWARA RAO <kotikondepati950@gmail.com>
2026-08-04 13:01:33 +0530

    PRACTICE1

---
+++ track/M02-Introduction -to-java/T02/Practice1.java
@@ -0,0 +1,28 @@
+public class Practice1 {
+    public static void main(String[] args) {
+        int number = -7;
+        int firstScore = 18;
+        int secondScore =25;
+        if (number > 0){
+            System.out.println("Number type: Positive");
+        }
+        else if (number < 0) {
+            System.out.println("Number type: Negative");
+        } else {
+            System.err.println("Number type: Zero");
+        }
+        if (number % 2 == 0) {
+            System.err.println("Parity: Even");
+        }
+        else {
+            System.err.println("Parity: Odd");
+        }
+        if (firstScore >= secondScore) {
+            System.err.println("Larger Score: " + firstScore);
+        }
+        else {
+            System.err.println("Larger Score: " + secondScore);
+        }    
+    }
+    
+}
