de8a2fa3d792b19de3d0f253d8293693c141e8b6
KONDEPATI KOTESWARA RAO <kotikondepati950@gmail.com>
2026-08-04 12:36:59 +0530

    print

---
+++ track/M02-Introduction -to-java/T02/Po1.java
@@ -0,0 +1,11 @@
+public class Po1 {
+    public static void main(String[] args){
+        System.out.println("Monday");
+        System.out.print("tuesday");
+        System.out.println("Wednesday");
+        System.out.println("Thursday");
+        System.out.print("Friday");
+        System.out.println("Saturday");
+        System.out.println("Sunday");
+    }
+}
