58f9848b3037495c43416e5674e9b9499690465e
KONDEPATI KOTESWARA RAO <kotikondepati950@gmail.com>
2026-08-04 12:28:33 +0530

    new Program

---
+++ track/M02-Introduction -to-java/T02/Po2.java
@@ -0,0 +1,16 @@
+
+public class Po2 {
+    public static void main(String[] args) {
+        int num = 12;
+        if (num > 0) {
+            System.out.println("number is positive");
+        }
+        else if (num < 0) {
+            System.out.println("number is negative");
+        }
+        else {
+            System.out.println("number is zero");
+        }
+    }
+
+}
