e2d2f171dc8887cf5821cab173e9798530eb6a19
KONDEPATI KOTESWARA RAO <kotikondepati950@gmail.com>
2026-08-06 12:47:17 +0530

    Day4 continue

---
+++ track/M02-Introduction -to-java/src/T02/Break2.java
@@ -0,0 +1,15 @@
+public class Break2 {
+    
+    public static void main(String[] args) {
+         for(int i =1;i<=2;i++){
+            for (int j = 1; j <+4; j++) {
+                 System.out.println("i: "+ i + "J: " + j);
+            if(i==6){
+            break;
+        }
+                
+            }
+           
+        }
+    }
+}
---
+++ track/M02-Introduction -to-java/src/T02/Skip2.java
@@ -0,0 +1,16 @@
+public class Skip2 {
+    public static void main(String[] args) {
+        for(int i =1;i<=2;i++){
+            for (int j = 1; j <=4; j++) {
+                if(j==3){
+            continue;
+        }
+                 System.out.println("i: "+ i + "J: " + j);
+            
+    }
+                
+       
+}
+    }
+    
+}
