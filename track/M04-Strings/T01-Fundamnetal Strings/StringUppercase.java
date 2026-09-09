f9b86db0f92ef0ad4d91e15df47c68b88ba79b18
KONDEPATI KOTESWARA RAO <kotikondepati950@gmail.com>
2026-09-09 16:55:05 +0530

    Add StringUppercase

---
+++ track/M04-Strings/T01-Fundamnetal Strings/StringUppercase.java
@@ -0,0 +1,11 @@
+
+public class StringUppercase {
+
+    public static void main(String[] args) {
+        String s1 = "java";
+        String res = s1.toUpperCase();
+
+        System.out.println(res);
+        System.out.println(s1);
+    }
+}
